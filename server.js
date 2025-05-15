ea69484536a929c64d452a3dcb938946a37317c6
silvvver <kerchflasher@gmail.com>
2025-05-15 23:28:17 +0300

    Добавлен CORS для поддержки фронта

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // server.js
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // 1. Импортируй CORS
 require('dotenv').config();
 
 const analyzeRoute = require('./routes/analyze');
 
 const app = express();
+
+// 2. Подключи CORS (лучше явно указать домен, но для тестов можно '*')
+app.use(cors({
+  origin: '*', // Лучше заменить на твой фронтовый домен в проде!
+  methods: ["GET", "POST"],
+}));
 
 // Статические файлы из public
 app.use(express.static('public'));
